8f6144ba8d735a6c85cd995e22851764914e3ca8
DESKTOP-1CKD89I\admin <dmjku16@gmail.com>
2022-08-16 15:51:25 +0900

    기존 백준 levelSix 파일 추가

---
+++ baekjoon/levelSix/PrintASCII.java
@@ -0,0 +1,26 @@
+package levelSix;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class PrintASCII {
+
+	public static void main(String[] args) throws IOException {
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+//		Scanner sc = new Scanner(System.in);
+		
+//		int num = sc.next().charAt(0);
+		
+//		int num = br.read();
+//		System.out.println(num);
+		
+		int num = System.in.read();
+		System.out.println(num);
+
+	}
+
+}
