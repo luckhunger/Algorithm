35022b55f3f6b3209a096e5208cd88536c001b31
DESKTOP-1CKD89I\admin <dmjku16@gmail.com>
2022-08-24 23:51:23 +0900

    백준 단계별로 풀어보기 6단계_2675(문자열 반복) BufferedReader 풀이방식 추가

--- baekjoon/levelSix/RepeatWord.java
+++ baekjoon/levelSix/RepeatWord.java
@@ -1,6 +1,8 @@
 package levelSix;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /*
@@ -17,31 +19,59 @@ import java.util.Scanner;
 public class RepeatWord {
 
 	public static void main(String[] args) throws IOException {
-		// Scanner 사용
-		Scanner sc = new Scanner(System.in);
-		// 테스트 케이스의 개수 > 총 몇 개의 반복횟수와 문자열을 입력받을지 작성
-		// 예를 들어 테스트 케이스가 2이면 >> 2 AAA / 3 BBB 라는 2개의 문자열을 입력 가능하다는 의미
-		int T = sc.nextInt();
-		int R;
-		char[] S;
+//		// Scanner 사용
+//		Scanner sc = new Scanner(System.in);
+//		// 테스트 케이스의 개수 > 총 몇 개의 반복횟수와 문자열을 입력받을지 작성
+//		// 예를 들어 테스트 케이스가 2이면 >> 2 AAA / 3 BBB 라는 2개의 문자열을 입력 가능하다는 의미
+//		int T = sc.nextInt();
+//		int R;
+//		char[] S;
+//		
+//		for(int i=0; i<T; i++) {
+//			// 반복 횟수
+//			R = sc.nextInt();
+//			// 문자열을 입력받고, 해당 문자열을 toCharArray()를 통해서 문자로 잘라 배열로 만들어줌 
+//			// nextLine()은 공백까지 읽어버리기에 사용 불가
+//			S = sc.next().toCharArray();
+//			
+//			// 입력한 문자열의 갯수만큼 반복
+//			for(int j=0; j<S.length; j++) {
+////				System.out.println("j : " + j);
+//				// 반복 횟수만큼 반복하며 해당 인덱스에 반복되는 동일한 문자 출력
+//				for(int z=0; z<R; z++) {
+////					System.out.println("z : " + z);
+//					System.out.print(S[j]);
+//				}
+//			}
+//			// 줄 넘기기 기능이 없어 임의로 작성
+//			System.out.println();
+//		}
+		
+		// BufferedReader 사용
+		// BufferedReader에는 throws Exception이 필수이다
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// readLine() : 한 줄을 읽어 문자열을 리턴해주는 메소드
+		// 리턴값이 String으로 고정되므로 다른 타입으로 입력받기 위해서는 형변환이 필수이다.
+		int T = Integer.parseInt(br.readLine());
 		
 		for(int i=0; i<T; i++) {
-			// 반복 횟수
-			R = sc.nextInt();
-			// 문자열을 입력받고, 해당 문자열을 toCharArray()를 통해서 문자로 잘라 배열로 만들어줌 
-			// nextLine()은 공백까지 읽어버리기에 사용 불가
-			S = sc.next().toCharArray();
+			// 입력받은 문자열을  공백 기준으로 분리
+			// 3 AAA > 3, AAA
+			String[] str = br.readLine().split(" ");
+			// 입력받은 문자열 중 맨 앞에는 반복 횟수이므로 분리하여 따로 저장
+			// ex) 3 AAA >> 3은 반복횟수(R)
+			int R = Integer.parseInt(str[0]);
+			// AAA는 문자열(S)
+			String S = str[1];
 			
-			// 입력한 문자열의 갯수만큼 반복
-			for(int j=0; j<S.length; j++) {
-//				System.out.println("j : " + j);
-				// 반복 횟수만큼 반복하며 해당 인덱스에 반복되는 동일한 문자 출력
+			for(int j=0; j<S.length(); j++) {
 				for(int z=0; z<R; z++) {
-//					System.out.println("z : " + z);
-					System.out.print(S[j]);
+					// 문자열 S를 charAt()을 통해 문자 하나씩 분리하여 반복 횟수만큼 출력
+					// ex) 인덱스 0인 문자 'A'를 3번 반복
+					System.out.print(S.charAt(j));
 				}
 			}
-			// 줄 넘기기 기능이 없어 임의로 작성
 			System.out.println();
 		}
 	}
