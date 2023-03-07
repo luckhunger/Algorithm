484fbf3b0bc97a846e073681581b4a1884e62bb0
DESKTOP-1CKD89I\admin <dmjku16@gmail.com>
2023-03-07 20:32:23 +0900

    수정

--- baekjoon/levelTwo/AlarmClocks.java
+++ baekjoon/levelTwo/AlarmClocks.java
@@ -19,6 +19,7 @@ import java.util.Scanner;
 시간을 나타낼 때, 불필요한 0은 사용하지 않는다.
 출력 : 첫째 줄에 상근이가 창영이의 방법을 사용할 때, 설정해야 하는 알람 시간을 출력한다. (입력과 같은 형태로 출력하면 된다.)
 */
+
 public class AlarmClocks {
 
 	public static void main(String[] args) {
