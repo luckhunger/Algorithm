4b5b46142e7463cc9b9c77dd447f03d64b2632c4
DESKTOP-1CKD89I\admin <dmjku16@gmail.com>
2023-03-16 19:54:55 +0900

    프로그래머스 Lv1 가운데 글자 가져오기

---
+++ programmers/levelOne/CenterString.java
@@ -0,0 +1,36 @@
+package levelOne;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+문제 : 2016년 1월 1일은 금요일입니다. 
+	 2016년 a월 b일은 무슨 요일일까요? 
+	  두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 
+	  요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT 입니다. 
+	  예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
+제한사항 : 2016년은 윤년입니다.
+	    2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+*/
+
+public class CenterString {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String s = st.nextToken();
+		String answer = "";
+		
+		// substring(a,b) : a부터 b-1의 위치까지의 문자를 리턴한다.
+		if(s.length()%2 == 0) {
+			answer = s.substring(s.length()/2 -1, s.length()/2+1);
+		}else {
+			answer = s.substring(s.length()/2, s.length()/2+1);
+		}
+		
+		System.out.println(answer);
+	}
+}
